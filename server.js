8edd667dfa1c45d291b34c893cd1a427b09879b0
lourod1987 <louart87@gmail.com>
2018-05-30 02:32:03 -0500

    messaging app now has data that persists using a mongo db server hosted through mLab and structured with mongoose

--- server.js
+++ server.js
@@ -3,30 +3,46 @@ var bodyParser = require('body-parser');
 var app = express();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
+var mongoose = require('mongoose');
 
 app.use(express.static(__dirname));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
 
-var messages = [
-    {name: "Matt", message: "Yo"},
-    {name: "Joy", message: "Ello"}
-];
+var dbUrl ='mongodb://admin:station24@ds239930.mlab.com:39930/messaging-app';
+
+var Message = mongoose.model('Message', {
+    name: String,
+    message: String
+});
 
 app.get('/messages', (req, res) => {
-    res.send(messages);
+    Message.find({}, (err, messages) => {
+        res.send(messages);
+    });
 });
 
 app.post('/messages', (req, res) => {
-    messages.push(req.body);
-    io.emit('message', req.body);
-    res.sendStatus(200);
+    var message = new Message(req.body);
+
+    message.save((err) => {
+        if (err) {
+            sendStatus(500);
+        }
+
+        io.emit('message', req.body);
+        res.sendStatus(200);
+    });
 });
 
 io.on('connection', (socket) => {
     console.log("a user connected");
 });
 
+mongoose.connect(dbUrl, (err) => {
+    console.log('mongo db connection', err);
+});
+
 var server = http.listen(3000, () => {
-    console.log('server is listening on port', server.address().port)
+    console.log('server is listening on port', server.address().port);
 });
